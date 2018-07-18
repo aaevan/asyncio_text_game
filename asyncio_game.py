398239a29381ba37ec6afb6f2ea142dded5afff1
aaevan <aaevan@gmail.com>
2018-07-17 21:30:10 -0400

    General cleanup, fixed snake() drawing.

--- asyncio_game.py
+++ asyncio_game.py
@@ -42,7 +42,6 @@ term = Terminal()
 def draw_box(top_left=(0, 0), x_size=1, y_size=1, filled=True, 
              tile=".", passable=True):
     """ Draws a box at the given coordinates."""
-
     x_tuple = (top_left[0], top_left[0] + x_size)
     y_tuple = (top_left[1], top_left[1] + y_size)
     if filled:
@@ -55,19 +54,16 @@ def draw_box(top_left=(0, 0), x_size=1, y_size=1, filled=True,
         map_dict[top_left].tile = tile
         map_dict[(x_tuple[1], y_tuple[1])].tile = tile
         for x in range(top_left[0], top_left[0] + x_size):
-            y = top_left[1]
-            map_dict[(x, y)].tile = tile
-            y = top_left[1] + y_size
-            map_dict[(x, y)].tile = tile
+            for y in [top_left[1], top_left[1] + y_size]:
+                map_dict[(x, y)].tile = tile
+            #map_dict[(x, y)].tile = tile
         for y in range(top_left[1], top_left[1] + y_size):
-            x = top_left[0]
-            map_dict[(x, y)].tile = tile
-            x = top_left[0] + x_size
-            map_dict[(x, y)].tile = tile
+            for x in [top_left[0], top_left[0] + x_size]:
+                map_dict[(x, y)].tile = tile
+            #map_dict[(x, y)].tile = tile
 
-async def draw_line(coord_a=(0, 0), coord_b=(5, 5), palette="*", passable=True):
+async def draw_line(coord_a=(0, 0), coord_b=(5, 5), palette="*", passable=True, blocking = False):
     """draws a line to the map_dict connecting the two given points."""
-    #TODO: fix problems with gaps in sharp up and down lines, i.e., 0,0 to 0, -5
     await asyncio.sleep(0)
     points = await get_line(coord_a, coord_b)
     for point in points:
@@ -76,7 +72,7 @@ async def draw_line(coord_a=(0, 0), coord_b=(5, 5), palette="*", passable=True):
         else:
             map_dict[point].tile = palette
         map_dict[point].passable = passable
-        map_dict[point].blocking = False
+        map_dict[point].blocking = blocking
 
 async def get_line(start, end):
     """Bresenham's Line Algorithm
@@ -133,14 +129,12 @@ async def get_line(start, end):
 async def is_clear_between(coord_a=(0, 0), coord_b=(5, 5)):
     """
     intended to be used for occlusion.
-
     show the tile that the first collision happened at but not the following tile
+    TODO: doors are not visible
     """
     await asyncio.sleep(.01)
-    history = []
+    open_space, walls, history = 0, 0, []
     points = await get_line(coord_a, coord_b)
-    open_space = 0
-    walls = 0
     for point in points:
         if map_dict[point].blocking == False:
             open_space += 1
@@ -229,16 +223,19 @@ def clear():
     _ = call('clear' if os.name =='posix' else 'cls')
 
 def draw_door(x, y, closed = True):
+    states = [('▮', False, True), ('▯', True, False)]
     if closed:
-        tile, passable, blocking = '+', False, True
+        tile, passable, blocking = states[0]
     else:
-        tile, passable, blocking = '_', True, False
+        tile, passable, blocking = states[1]
     map_dict[(x, y)].tile = tile
     map_dict[(x, y)].passable = passable
     map_dict[(x, y)].blocking = blocking
 
 def map_init():
     clear()
+    draw_box(top_left=(-25, -25), x_size=50, y_size=50, tile="░")
+    """
     sow_texture(20, 20, radius=50, seeds=500, color_num=7)
     draw_box(top_left=(-5, -5), x_size=10, y_size=10, tile="░")
     map_dict[(3, 3)].tile = '☐'
@@ -252,13 +249,15 @@ def map_init():
     connect_with_passage(20, 20, 35, 20)
     connect_with_passage(0, 0, 17, 17)
     connect_with_passage(39, 20, 41, 20)
-    draw_door(7, 15)
+    draw_door(7, 16)
     draw_door(0, 5)
     draw_door(14, 17)
     draw_door(25, 20)
     draw_door(25, 20)
     draw_door(29, 20)
+    draw_door(41, 20)
     sow_texture(55, 25, radius=5, seeds=10, palette=":,~.:\"`", color_num=1, passable=True)
+    """
 
 def isData(): 
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []) 
@@ -267,7 +266,6 @@ async def handle_input(key):
     """interpret keycodes and do various actions.
     implement pushing a box around 
     """
-
     await asyncio.sleep(0)  
     x_shift, y_shift = 0, 0 
     x, y = (actor_dict['player'].x_coord,
@@ -282,8 +280,6 @@ async def handle_input(key):
         asyncio.ensure_future(vine_grow(start_x=x, start_y=y)),
     if key in 'Ee':
         asyncio.ensure_future(draw_line((0, 0), (x, y))),
-    if key in 'Pp':
-        asyncio.ensure_future(debug_grid())
     if key in ' ':
         asyncio.ensure_future(toggle_doors()),
     if map_dict[(shifted_x, shifted_y)].passable:
@@ -298,7 +294,6 @@ async def handle_input(key):
 async def debug_grid(x_print_coord=0, y_print_coord=6):
     """
     print a grid of ones and zeroes running is_clear_between on the area surrounding player location.
-    TODO: make this work??.
     """
     await asyncio.sleep(0)
     player_x, player_y = (actor_dict['player'].x_coord,
@@ -309,18 +304,15 @@ async def debug_grid(x_print_coord=0, y_print_coord=6):
             if (x_coord, y_coord) == (player_x, player_y):
                 output_array[y_index][x_index] = '@'
             else:
-                output_array[y_index][x_index] = int(await is_clear_between((player_x, player_y), (player_x + x_coord, player_y + y_coord)))
+                target_tuple = (player_x + x_coord, player_y + y_coord)
+                output_array[y_index][x_index] = int(await is_clear_between((player_x, player_y), target_tuple))
     with term.location(x_print_coord, y_print_coord):
         for row in output_array:
             print(''.join([str(i) for i in row]))
-        #print(output_array)
 
-
-#async def view_tile(x_offset=1, y_offset=1, distance_effects = True, threshold = 12):
 async def view_tile(x_offset=1, y_offset=1, threshold = 12):
     """ handles displaying data from map_dict """
-    #noise = "░" * 1 + " " * 5
-    noise = " " * 5
+    noise_palette = " " * 5
     distance = sqrt(abs(x_offset)**2 + abs(y_offset)**2) #
     await asyncio.sleep(random()/5 * distance)
     middle_x, middle_y = (int(term.width / 2 - 2), 
@@ -328,7 +320,7 @@ async def view_tile(x_offset=1, y_offset=1, threshold = 12):
     previous_actor, previous_tile, actor = None, None, None
     print_location = (middle_x + x_offset, middle_y + y_offset)
     last_printed = ' '
-    while(1):
+    while True:
         await asyncio.sleep(.01)
         player_x, player_y = (actor_dict['player'].x_coord,
                               actor_dict['player'].y_coord)
@@ -345,54 +337,23 @@ async def view_tile(x_offset=1, y_offset=1, threshold = 12):
             if await is_clear_between((player_x, player_y), (x, y)):
                 if actor:
                     print_choice = actor
-                    #with term.location(*print_location):
-                        #if actor != last_printed:
-                            #print(actor)
-                            #last_printed = actor
                 else:
                     print_choice = tile
-                    #with term.location(*print_location):
-                        #if tile != last_printed:
-                            #print(tile)
-                            #last_printed = tile
             else:
                 print_choice = ' '
-                #with term.location(*print_location):
-                    #if last_printed != ' ':
-                        #print(' ')
-                        #last_printed = ' '
         else:
-            print_choice = choice(noise)
-            #with term.location(*print_location):
-                #if last_printed != noise_choice:
-                    #print(noise_choice)
-                    #last_printed = noise_choice
+            print_choice = choice(noise_palette)
         with term.location(*print_location):
             if last_printed != print_choice:
                 print(print_choice)
                 last_printed = print_choice
             await asyncio.sleep(distance * .015)
-        #previous_tile, previous_actor = tile, actor
-
-async def noise_tile(x_offset, y_offset, threshhold=10):
-    """ breaks out noisy tile code into a separate routine 
-
-    flickers in and out depending on how distant it is from player
-
-    probability of showing new information (rather than grey last known state)
-    is based on euclidean distance from player
-    """
-    pass
-    #noise = "▓▒░░░░▖▗▘▙▚▛▜▝▞▟"
-    #distance = sqrt(abs(x_offset)**2 + abs(y_offset)**2) #
-    #await asyncio.sleep(distance/10) #changing delay by distance creates an expanding circle
-    #random_noise = choice(noise)
 
 async def shaded_tile(x=15, y=15, on_percent=.5, rate=.01, tile="#"):
     await asyncio.sleep(0)
     wait_on = rate * on_percent
     wait_off = rate - (rate * on_percent)
-    while(1):
+    while True:
         with term.location(x, y):
             print(tile)
         await asyncio.sleep(wait_on)
@@ -403,7 +364,7 @@ async def shaded_tile(x=15, y=15, on_percent=.5, rate=.01, tile="#"):
 async def timer(x_pos=0, y_pos=10, time_minutes=0, time_seconds=5, resolution=1):
     await asyncio.sleep(0)
     timer_text = str(time_minutes).zfill(2) + ":" + str(time_seconds).zfill(2)
-    while(1):
+    while True:
         await asyncio.sleep(resolution)
         with term.location(x_pos, y_pos):
             print(term.red(timer_text))
@@ -443,8 +404,6 @@ async def wander(x_current, y_current, name_key):
     """ 
     randomly increments or decrements x_current and y_current
     if square to be moved to is passable
-   
-    TODO:implement movements to be made as a palette to be pulled from, using 0-9
     """
     await asyncio.sleep(0)
     x_move, y_move = randint(-1, 1), randint(-1, 1)
@@ -485,53 +444,63 @@ async def snake(start_x=0, start_y=0, speed=.1, head="0", length=5, name_key="sn
     when the head moves to a new cell, the next segment in line takes it's coordinates.
     """
     await asyncio.sleep(0)
+    #initialize segment locations all to starting coordinates:
     segment_locations = [(start_x, start_y)] * length
     rand_direction = randint(1, 4)
     snake_segments = {(1, 2):'╭', (4, 3):'╭', (2, 3):'╮', (1, 4):'╮', (1, 1):'│', (3, 3):'│', 
-            (3, 4):'╯', (2, 1):'╯', (3, 2):'╰', (4, 1):'╰', (2, 2):'─', (4, 4):'─', }
+                      (3, 4):'╯', (2, 1):'╯', (3, 2):'╰', (4, 1):'╰', (2, 2):'─', (4, 4):'─', 
+                      (4, 2):'0', (2, 4):'0', (1, 3):'0', (3, 1):'0'}
+    new_x, new_y = 0, 0 
     movement_tuples = {1:(0, -1), 2:(1, 0), 3:(0, 1), 4:(-1, 0)}
-    segment_names = []
     movement_history = [1] * 10
+    #initialize segment actors:
+    segment_names = []
     for number, segment_coord in enumerate(segment_locations):
         segment_names.append("{}_seg_{}".format(name_key, number))
-        actor_dict[(segment_names)] = Actor(x_coord=segment_coord[0], y_coord=segment_coord[1])
-    new_x, new_y = 0, 0 
+        actor_dict[(segment_names[-1])] = Actor(x_coord=segment_coord[0], y_coord=segment_coord[1])
+    print(segment_names)
     while 1:
-        await asyncio.sleep(speed)
+        #await asyncio.sleep(speed)
+        await asyncio.sleep(.2)
         #deleting all traces of the snake segments from the map_dict's actor list:
         for name_key, coord in zip(segment_names, segment_locations):
             if name_key in map_dict[coord].actors:
                 del map_dict[coord].actors[name_key]
         segment_locations.pop()
-        #creating a new head location:
+        #creating a new head location that doesn't overlap with existing segments:
         while (new_x, new_y) in segment_locations:
             rand_direction = randint(1, 4)
-            movement_history.pop()
-            movement_history.insert(0, rand_direction)
             rand_direction_tuple = movement_tuples[rand_direction]
-            await asyncio.sleep(0)
             new_x, new_y = (segment_locations[0][0] + rand_direction_tuple[0],
                             segment_locations[0][1] + rand_direction_tuple[1],)
+        movement_history.pop()
+        movement_history.insert(0, rand_direction)
         new_head_coord = (new_x, new_y)
         segment_locations.insert(0, new_head_coord)
         #write the new locations of each segment to the map_dict's tiles' actor lists
+        counter = 0
         for name_key, coord in zip(segment_names, segment_locations):
+            with term.location(0, 13):
+                print(movement_history)
+            with term.location(0, 15 + counter):
+                print(name_key, coord)
             actor_dict[(name_key)].x_coord = coord[0]
             actor_dict[(name_key)].y_coord = coord[1]
-            #TODO: fix snake tile display
             #segment_tile_key = (movement_history[int(name_key[-1])], movement_history[int(name_key[-1]) + 1])
-            segment_tile_key = 'O'
-            with term.location(0, 3):
-                print(segment_tile_key)
-            if segment_tile_key in snake_segments:
-                actor_dict[(name_key)].tile = snake_segments[segment_tile_key]
+            segment_tile_key = (movement_history[counter], movement_history[counter - 1])
+            with term.location(24, 15 + counter):
+                print(movement_history[counter], movement_history[counter - 1])
+            if segment_tile_key == (0, -1):
+                actor_dict[(name_key)].tile = '1'
             else:
-                actor_dict[(name_key)].tile = 'o'
+                #assign tile:
+                with term.location(0, 30 + counter):
+                    print(segment_tile_key)
+                actor_dict[(name_key)].tile = snake_segments[segment_tile_key]
+                with term.location(21, 15 + counter):  ###
+                    print(actor_dict[(name_key)].tile) ###
             map_dict[coord].actors[name_key] = name_key
-        with term.location(0, 0):
-            print(segment_locations)
-        with term.location(0, 1):
-            print(movement_history)
+            counter += 1
 
 async def basic_actor(start_x=0, start_y=0, speed=.2, tile="*", 
         movement_function=wander, name_key="test"):
@@ -555,7 +524,7 @@ async def constant_update_tile(x_offset=0, y_offset=0, tile=term.red('@')):
     await asyncio.sleep(1/30)
     middle_x, middle_y = (int(term.width / 2 - 2),
                           int(term.height / 2 - 2))
-    while(1):
+    while True:
         await asyncio.sleep(1/30)
         with term.location(middle_x, middle_y):
             print(tile)
@@ -567,12 +536,12 @@ async def toggle_doors():
     for door in door_dirs:
         door_coord_tuple = (x + door[0], y + door[1])
         door_state = map_dict[door_coord_tuple].tile 
-        if door_state == "+":
-            map_dict[door_coord_tuple].tile = '_'
+        if door_state == "▮":
+            map_dict[door_coord_tuple].tile = '▯'
             map_dict[door_coord_tuple].passable = True
             map_dict[door_coord_tuple].blocking = False
-        elif door_state == '_':
-            map_dict[door_coord_tuple].tile = '+'
+        elif door_state == '▯':
+            map_dict[door_coord_tuple].tile = '▮'
             map_dict[door_coord_tuple].passable = False
             map_dict[door_coord_tuple].blocking = True
 
@@ -589,11 +558,10 @@ async def vine_grow(start_x=0, start_y=0, actor_key="vine",
                 (3, 4):'╯', (2, 1):'╯', (3, 2):'╰', (4, 1):'╰', (2, 2):'─', (4, 4):'─', }
     exclusions = {(2, 4), (4, 2), (1, 3), (3, 1), }
     vines = [term.green(i) for i in "┌┐└┘─│"]
-    
     prev_dir, next_dir = randint(1, 4), randint(1, 4)
     movement_tuples = {1:(0, -1), 2:(1, 0), 3:(0, 1), 4:(-1, 0)}
     next_tuple = movement_tuples[next_dir]
-    while(1):
+    while True:
         await asyncio.sleep(rate)
         next_dir = randint(1, 4)
         while (prev_dir, next_dir) in exclusions:
@@ -609,7 +577,7 @@ async def vine_grow(start_x=0, start_y=0, actor_key="vine",
             return
 
 async def run_every_n(sec_interval=3, repeating_function=vine_grow, args=() ):
-    while(1):
+    while True:
         await asyncio.sleep(sec_interval)
         x, y = (actor_dict['player'].x_coord,
                 actor_dict['player'].y_coord,)
@@ -625,7 +593,7 @@ async def sin_loop(state_dict_key="testsin", update_speed=.1, length=10):
 async def track_actor_location(state_dict_key="player", actor_dict_key="player", update_speed=.1, length=10):
     await asyncio.sleep(0)
     actor_coords = None
-    while(1):
+    while True:
         with term.location(5, 5):
             print("{:8}".format(str(actor_coords)))
         await asyncio.sleep(update_speed)
@@ -638,7 +606,7 @@ async def readout(x_coord=0, y_coord=7, listen_to_key=None, update_rate=.1, floa
     modify to be able to display status bars, ie, 'LIFE:█████░░░░░'
     """
     await asyncio.sleep(0)
-    while(1):
+    while True:
         await asyncio.sleep(0)
         key_value = state_dict[listen_to_key]
         if type(key_value) is float:
@@ -669,6 +637,7 @@ def main():
     loop = asyncio.new_event_loop()
     loop.create_task(get_key())
     loop.create_task(view_init(loop))
+    loop.create_task(snake())
     loop.create_task(basic_actor(*(10, 10), speed=.5, movement_function=seek, tile="Ø", name_key="test_seeker"))
     loop.create_task(constant_update_tile())
     loop.create_task(track_actor_location())
